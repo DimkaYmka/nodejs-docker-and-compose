1c0f6c6225438e800c25398e2c63af08df1ab5bf
DimkaYmka <mungalov00@icloud.com>
2025-02-22 17:12:38 +0300

    front dock

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -8,11 +8,11 @@ module.exports = {
       max_memory_restart: "1G",
       env: {
         NODE_ENV: "production",
-        DB_HOST: "postgres",  // Измените с "localhost" на "postgres"
-        DB_PORT: 5432,
-        DB_USER: "student",
-        DB_PASSWORD: "student",
-        DB_NAME: "nest_project",
+        POSTGRES_HOST: "postgres",  // Измените с "localhost" на "postgres"
+        POSTGRES_PORT: 5432,
+        POSTGRES_USER: "student",
+        POSTGRES_PASSWORD: "student",
+        POSTGRES_DB: "nest_project",
         JWT_SECRET: "8f6dc4c1c9e3bf0da3e9929511f269c67897595f5c996a78670b6742d8445",
         JWT_EXPIRATION_TIME: 360000,
         PORT: 3000
